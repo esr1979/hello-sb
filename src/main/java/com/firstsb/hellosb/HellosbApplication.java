3020e2fcb939d50eed7327682b1d3fc65cf96183
ESANCH69 <esanch69@ford.com>
2019-05-13 11:36:48 +0200

    Initial version
    Project created with Spring Boot Initializr

---
+++ src/main/java/com/firstsb/hellosb/HellosbApplication.java
@@ -0,0 +1,13 @@
+package com.firstsb.hellosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellosbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstsb/hellosb/HellosbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.firstsb.hellosb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellosbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
