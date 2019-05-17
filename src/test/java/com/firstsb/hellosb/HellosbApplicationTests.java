15843307faffd1f462805672732e0354fe0b4e5a
ESANCH69 <esanch69@ford.com>
2019-05-17 10:35:49 +0200

    Corrections in test to check the appropiate port

--- src/test/java/com/firstsb/hellosb/HellosbApplicationTests.java
+++ src/test/java/com/firstsb/hellosb/HellosbApplicationTests.java
@@ -3,30 +3,39 @@ package com.firstsb.hellosb;
 import com.firstsb.hellosb.persistence.model.Cosa;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
 import io.restassured.response.Response;
 
+import io.restassured.specification.RequestSpecification;
+import javafx.application.Application;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = HellosbApplication.class)
 public class HellosbApplicationTests {
 
 	private static final Logger log = LoggerFactory.getLogger(HellosbApplicationTests.class);
-
-	private static final String API_ROOT
-			= "http://localhost:8081";
-
 
 	private Cosa createRandomCosa() {
 		final Cosa cosa = new Cosa("","");
@@ -35,17 +44,16 @@ public class HellosbApplicationTests {
 		return cosa;
 	}
 
-	private String createCosaAsUri(Cosa cosa) {
-		final Response response = RestAssured.given()
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.body(cosa)
-				.post(API_ROOT);
-		return API_ROOT + "/" + response.jsonPath()
-				.get("id");
+	@LocalServerPort
+	int port;
+
+	@Before
+	public void setUp() {
+		RestAssured.port = port;
 	}
-	
-	
+
 	@Test
+	@Order(1)
 	public void contextLoads() {
 
 		log.info("CONTEXT TEST LOAD HAS BEEN EXECUTED");
@@ -53,6 +61,7 @@ public class HellosbApplicationTests {
 	}
 
 	@Test
+	@Order(2)
 	public void createCosa() {
 
 		log.info("LET'S SEE IF WE CAN CREATE A THING");
@@ -64,8 +73,12 @@ public class HellosbApplicationTests {
 	}
 
 	@Test
+	@Order(3)
 	public void testEndpoint() {
-		Response response = RestAssured.get(API_ROOT);
+
+		log.info("CONNECTING TO ENDPOINT");
+		Response response = RestAssured.get("http://localhost");
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
 
 	}
 
