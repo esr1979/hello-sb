bf517f3055af355c97daa6ac9c17d9e12430d51a
Enrique Sánchez <esr1979@gmail.com>
2019-05-19 09:27:43 +0200

    Completion of tests. Next day different things

--- src/test/java/com/firstsb/hellosb/HellosbApplicationTests.java
+++ src/test/java/com/firstsb/hellosb/HellosbApplicationTests.java
@@ -28,6 +28,7 @@ import javax.print.attribute.standard.Media;
 import java.util.List;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 
 
 @RunWith(SpringRunner.class)
@@ -63,14 +64,14 @@ public class HellosbApplicationTests {
 	}
 
 	@Test
-	public void Step1_contextLoads() {
+	public void Step01_contextLoads() {
 
 		log.info("CONTEXT TEST LOAD HAS BEEN EXECUTED");
 
 	}
 
 	@Test
-	public void Step2_createCosa() {
+	public void Step02_createCosa() {
 
 		log.info("LET'S SEE IF WE CAN CREATE A THING");
 		Cosa cosa = new Cosa("", "");
@@ -81,7 +82,7 @@ public class HellosbApplicationTests {
 	}
 
 	@Test
-	public void Step3_testEndpoint() {
+	public void Step03_testEndpoint() {
 
 		log.info("CONNECTING TO ENDPOINT");
 		Response response = RestAssured.get("http://localhost");
@@ -90,7 +91,7 @@ public class HellosbApplicationTests {
 	}
 
 	@Test
-	public void Step4_createCosasThere(){
+	public void Step04_createCosasThere(){
 
 		log.info("CREATION OF NEW COSA");
 		Cosa cosa = new Cosa("","");
@@ -115,7 +116,7 @@ public class HellosbApplicationTests {
 		Assert.assertEquals(HttpStatus.CREATED.value(), response.getStatusCode());
 
 		cosa = createRandomCosa();
-		log.info("NAME --> " + cosa.getNombre());
+		log.info(" --> " + cosa.getNombre());
 		log.info("DESCRIPTION --> " + cosa.getDescripcion());
 		log.info("PUTTING THE COSA THERE 3");
 		response = RestAssured.given()
@@ -133,7 +134,7 @@ public class HellosbApplicationTests {
 	}
 
 	@Test
-	public void Step_5_getAllCosas() {
+	public void Step_05_getAllCosas() {
 
 		Response response = RestAssured.get(API_ROOT);
 		String location = API_ROOT + "/" + response.jsonPath().get("id");
@@ -144,7 +145,7 @@ public class HellosbApplicationTests {
 	}
 
 	@Test
-	public void Step_6_createCosaThereAsUri() {
+	public void Step_06_createCosaThereAsUri() {
 
 		Cosa cosa = createRandomCosa();
 		String location = createCosaAsUri(cosa);
@@ -160,7 +161,7 @@ public class HellosbApplicationTests {
 	}
 
 	@Test
-	public void Step_7_getCosaByName() {
+	public void Step_07_getCosaByName() {
 
 		Cosa cosa = createRandomCosa();
 		createCosaAsUri(cosa);
@@ -186,5 +187,86 @@ public class HellosbApplicationTests {
 				.size() > 0);
 	}
 
+	@Test
+	public void Step_08_getNotExistentCosa(){
+
+		String cosaToSearch = API_ROOT + "/" + randomNumeric(4);
+		Response response = RestAssured.get(cosaToSearch);
+		log.info("LOOKING FOR THIS COSA --> " + cosaToSearch);
+		Assert.assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode());
+
+	}
+
+	@Test
+	public void Step_09_createInvalidCosa() {
+
+		Cosa cosa = createRandomCosa();
+		cosa.setDescripcion(null);
+		Response response = RestAssured.given()
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.body(cosa)
+				.post(API_ROOT);
+		log.info("WHOLE BODY --> " + response.getBody().asString());
+		Assert.assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCode());
+
+
+	}
+
+	@Test
+	public void Step_10_createUpdateCosa() {
+		Cosa cosa = createRandomCosa();
+		String location = createCosaAsUri(cosa);
+		log.info("LOCATION IS -->" + location);
+		cosa.setId(Long.parseLong(location.split("api/cosas/")[1]));
+		cosa.setDescripcion("Es una cosa de goma");
+		cosa.setNombre("Goma");
+
+		Response response = RestAssured.given()
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.body(cosa)
+				.put(location);
+
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+
+		response = RestAssured.get(location);
+
+		log.info("THE ACTUALIZED THING IS --> " + response.getBody().asString());
+
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+		Assert.assertEquals("Es una cosa de goma", response.jsonPath().get("descripcion"));
+		Assert.assertEquals("Goma", response.jsonPath().get("nombre"));
+
+	}
+
+	@Test
+	public void Step_11_createDeleteCosa() {
+		Cosa cosa = createRandomCosa();
+		String location = createCosaAsUri(cosa);
+
+		Response response = RestAssured.get(API_ROOT);
+		String locationBefore = API_ROOT + "/" + response.jsonPath().get("id");
+		log.info("WE HAVE READ THE FOLLOWING PIECE OF SHIT --> " + response.getBody().asString());
+		log.info("THE COSAS WE HAVE READ ARE LOCATED IN --> " + locationBefore);
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+
+		response = RestAssured.delete(location);
+
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+
+		response = RestAssured.get(location);
+
+		log.info("WE HAVE CREATED AND DELETED --> " + location);
+
+		Assert.assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode());
+
+		response = RestAssured.get(API_ROOT);
+		String locationAfter = API_ROOT + "/" + response.jsonPath().get("id");
+		log.info("WE HAVE READ THE FOLLOWING PIECE OF SHIT --> " + response.getBody().asString());
+		log.info("THE COSAS WE HAVE READ ARE LOCATED IN --> " + locationAfter);
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+
+	}
+	
+	
 
 }
